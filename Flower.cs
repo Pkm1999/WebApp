1ba93e028a6fe38e6589d511992ce5d3d4a72e20
Prajwal Mowade <98078847+Pkm1999@users.noreply.github.com>
2022-01-26 19:41:05 +0530

    flower file added
    flower.cs file added

---
+++ Flower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySqlConnectivity.Models
+{
+    public class Flower
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public Flower(int id, string name, string color)
+        {
+            Id = id;
+            Name = name;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return "Id : "+Id+" Name : "+Name+" Color : "+Color;
+        }
+    }
+}
